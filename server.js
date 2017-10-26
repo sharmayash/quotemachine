0e424ae271dd7c630d521bf2b56526296b1b1307
yash sharma <sharmayash44ys@gmail.com>
2017-10-27 00:14:41 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+//set the port 
+var port = process.env.PORT || 5000
+app.use(express.static(_dirname));
+
+//routes
+
+app.get("/", function(req, res) {
+    res.render("index");
+})
+
+app.listen(port, function() {
+    console.log("app is satrted")
+})
