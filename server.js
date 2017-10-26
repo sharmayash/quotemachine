da020c98d147f14d3996c63cef2728aaee472c2c
yash sharma <sharmayash44ys@gmail.com>
2017-10-27 00:58:23 +0530

    try baba

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 //set the port 
 var port = process.env.PORT || 5000
 app.use(express.static(_dirname + "/public"));
-
+app.use(favicon(_dirname + '/public/favicon.ico'));
 //routes
 
 app.get("/", function(req, res) {
